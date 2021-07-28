b062d72a7fb4e44b56ad1a09639f35c57cc23318
liangqi <752985570@qq.com>
2021-07-28 13:37:44 +0800

    update: first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import routes from './router'
+import { Tabbar, TabbarItem, Form, Field, Button } from "vant";
+Vue.use(VueRouter).use(Tabbar).use(TabbarItem).use(Form).use(Field).use(Button)
 
+import 'lib-flexible'
+import '@/style/base.css'
+import '@/style/index.scss'
 Vue.config.productionTip = false
-
+const router = new VueRouter({
+  routes,
+})
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
